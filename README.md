d28cb285bd23a89596d1e01d63a67aa34eb5386d
Thijs Clowting <thijs@clowting.me>
2015-06-09 10:50:22 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# twz
-A scheduling application made for TWZ.
+# TWZ Scheduling
+A scheduling application made for TWZ during the HBO-ICT Project week at HZ University of Applied Sciences.
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/stylesheets/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+
+    </body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/javascripts/bootstrap.min.js"></script>
+
+</html>
