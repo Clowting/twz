5f52c9dddba26e1f528520e4d4493178129387eb
MasterQueue <q.geervliet@hotmail.nl>
2015-06-15 16:39:43 +0200

    Blank

--- pages/blank.php
+++ pages/blank.php
@@ -42,9 +42,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Tables <button type="button" class="btn btn-primary">Primary</button> <button type="button" class="btn btn-primary">Primary</button></h1>
-                <button type="button" class="btn btn-primary">Primary</button>
-                <button type="button" class="btn btn-primary">Primary</button>
+                <h1 class="page-header">Tables <button type="button" class="btn btn-primary">Bewerken</button> <button type="button" class="btn btn-primary">Verwijderen</button></h1>
             </div>
             <!-- /.col-lg-12 -->
         </div>
