7bc8ca15618969d21eca8d249da615f7819621b7
Thijs vd Eerden <thijsvdeerden@gmail.com>
2015-06-15 14:03:49 +0200

    nav fixes < .php

--- pages/index.php
+++ pages/index.php
@@ -7,9 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <meta name="author" content="Volt-Ossom">
+    <title>TWZ Admin</title>
 
     <!-- Main CSS -->
     <link href="../stylesheets/styles.css" rel="stylesheet">
