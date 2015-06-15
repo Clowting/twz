251cb77bf5bb02dcc84b8dc80d009f2d9025f3f3
MasterQueue <q.geervliet@hotmail.nl>
2015-06-15 16:36:15 +0200

    Blank

--- pages/blank.php
+++ pages/blank.php
@@ -43,6 +43,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Tables</h1>
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary">Primary</button>
             </div>
             <!-- /.col-lg-12 -->
         </div>
