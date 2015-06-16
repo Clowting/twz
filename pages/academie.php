67201c539015e0d80a7b9791fb179d97db744190
Thijs vd Eerden <thijsvdeerden@gmail.com>
2015-06-16 15:23:42 +0200

    academie.php verbeterd == auth

--- pages/academie.php
+++ pages/academie.php
@@ -1,13 +1,13 @@
 <?php
 require_once '../lib/connectdb.php';
 require_once '../lib/functions.php';
-require_once '../lib/requireAuth.php';
-require_once '../lib/requireSession.php';
-require_once '../lib/requireAdmin.php';
+//require_once '../lib/requireAuth.php';
+//require_once '../lib/requireSession.php';
+//require_once '../lib/requireAdmin.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-.
+
 <head>
 
     <meta charset="utf-8">
