6155e57cf09aa95018444f12507ce59ed270d27e
MasterQueue <q.geervliet@hotmail.nl>
2015-06-16 09:53:33 +0200

    Blank

--- pages/survedit.php
+++ pages/survedit.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Startklaar
+ * Date: 16-6-2015
+ * Time: 09:52
+ */
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,8 +34,8 @@
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
@@ -43,12 +51,6 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Tables
-                    <form action="survedit.php">
-                    <button type="button" class="btn btn-primary">Bewerken</button>
-                    </form>
-                        <form action="survdel.php">
-                    <button type="submit" class="btn btn-danger">Verwijderen</button>
-                        </form>
                 </h1>
             </div>
             <!-- /.col-lg-12 -->
