54f1e58edf7c2faa236fbca2034cc08ec283ec66
Thijs Clowting <thijs@clowting.me>
2015-06-16 12:25:06 +0200

    Basic authentication finished.

--- pages/login.php
+++ pages/login.php
@@ -58,7 +58,7 @@ require_once '../lib/functions.php';
                                     if($logout) {
                                         echo '<div class="alert alert-success" role="alert">U bent succesvol uitgelogd.</div>';
                                     } else {
-                                        echo '<div class="alert alert-warning" role="alert">Er is iets fout gegaan bij het uitloggen.</div>';
+                                        echo '<div class="alert alert-danger" role="alert">Er is iets fout gegaan bij het uitloggen.</div>';
                                     }
                                 }
                             }
@@ -69,9 +69,13 @@ require_once '../lib/functions.php';
                                 } else {
                                     $login = $auth->login($_POST['email'], $_POST['password']);
                                 }
-                                print_r($login['hash']);
-                                setcookie($config->cookie_name, $login['hash'], $login['expire']);
-                                header('Location: index.php');
+                                
+                                if($login['error'] == 0) {
+                                    setcookie($config->cookie_name, $login['hash'], $login['expire']);
+                                    header('Location: index.php');
+                                } else {
+                                    echo '<div class="alert alert-warning" role="alert">' . $login['message'] . '</div>';
+                                }
                             }
 
                         ?>
