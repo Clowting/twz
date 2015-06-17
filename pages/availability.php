93460ad55c737e479c36c26a12c4da5059be73af
Thijs vd Eerden <thijsvdeerden@gmail.com>
2015-06-17 12:34:08 +0200

    availability

---
+++ pages/availability.php
@@ -0,0 +1,102 @@
+<?php
+require_once '../lib/connectdb.php';
+require_once '../lib/functions.php';
+require_once '../lib/requireAuth.php';
+require_once '../lib/requireSession.php';
+require_once '../lib/requireUser.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>TWZ - beschikbaarheid</title>
+
+    <?php include_once "../includes/head.php"; ?>
+
+</head>
+<body>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include_once "../includes/nav.php"; ?>
+
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Surveillanten
+                    <small>Overzicht</small>
+                    <a href="survcreate.php" role="button" class="btn btn-primary align-right">Toevoegen</a>
+                </h1>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel panel-default">
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                <tr>
+                                    <th>Maandag</th>
+                                    <th>Dinsdag</th>
+                                    <th>Woensdag</th>
+                                    <th>Donderdag</th>
+                                    <th>Vrijdag</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+<!-- DataTables JavaScript -->
+<script src="../bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="../dist/js/sb-admin-2.js"></script>
+
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+</script>
+
+</body>
+
+</html>
